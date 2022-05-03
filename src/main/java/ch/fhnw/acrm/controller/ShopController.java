e9eeb82381b5a7574b8034281edb6287eafb547b
Jan Wilhelm <jan.wilhelm@students.fhnw.ch>
2022-05-03 17:04:34 +0200

    /shop stuff

---
+++ src/main/java/ch/fhnw/acrm/controller/ShopController.java
@@ -0,0 +1,26 @@
+package ch.fhnw.acrm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/shop")
+
+public class ShopController {
+
+    @GetMapping
+    public String getShopView(){
+        return "shop/shop.html";
+    }
+
+    @GetMapping("/create")
+    public String getDashboardCreateView(){
+        return "shop/dashboard.html";
+    }
+
+    @GetMapping("/edit")
+    public String getCustomerEditView(){
+        return "shop/settings";
+    }
+}
