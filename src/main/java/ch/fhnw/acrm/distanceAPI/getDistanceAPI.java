ac4111927a5aff72f7425d4f1aca7c91116f78eb
kenanahmetasevic <kenan.ahmetasevic@students.fhnw.ch>
2022-05-17 20:48:14 +0200

    Update Distance API connection

--- src/main/java/ch/fhnw/acrm/distanceAPI/getDistanceAPI.java
+++ src/main/java/ch/fhnw/acrm/distanceAPI/getDistanceAPI.java
@@ -3,48 +3,62 @@ package ch.fhnw.acrm.distanceAPI;
 
 import java.net.URI;
 import java.net.http.*;
-import java.net.http.HttpRequest;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-
 import java.net.http.HttpResponse;
 
 //
 public class getDistanceAPI {
 
         private static final String API_KEY = "AIzaSyCsceVMrBAdupkGbu0zM9jOOBrz2s5wEwY";
-        public static float[][] distances;
+        private static String destination;
+        //public static float[][] distances;
+        public static final String ORIGIN_ADRESS = "Basel";
 
         //download data
         public static void getData(String source, String destination) throws Exception {
+
+                long distance = 1;
 
                 HttpClient client = HttpClient.newHttpClient();
                 HttpRequest request = HttpRequest.newBuilder()
                         .uri(java.net.URI.create("https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + source + "&destinations=" + destination + "&key=" + API_KEY))
                         .build();
                 client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                        .thenApply(HttpResponse::body)
-                        .thenAccept(System.out::println);
+                        .thenApply(HttpResponse::body);
 
-                System.out.println(client.send(request, HttpResponse.BodyHandlers.ofString()).body());
+                String response = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
 
+
+                JSONParser jp = new JSONParser();
+                JSONObject jo = (JSONObject) jp.parse(response);
+                JSONArray ja = (JSONArray) jo.get("rows");
+                jo = (JSONObject) ja.get(0);
+                ja = (JSONArray) jo.get("elements");
+                jo = (JSONObject) ja.get(0);
+                JSONObject je = (JSONObject) jo.get("distance");
+
+                distance = (long) je.get("value");
+
+                System.out.println(distance + " m");
+        }
+        public static void setDestination(String des) {
+
+                destination = des;
+        }
+        public static String getDestination() {
+
+                return destination;
         }
 
-        public static void main(String[] args) {
+        public static void main(String[] args) throws Exception {
 
-                String source = "Banglore";
-                String destination = "NewDehli";
+                setDestination("StGallen");
 
-                try {
-                        getData(source, destination);
-                } catch (Exception e) {
-                        e.printStackTrace();
-                }
-
-
-
+                getData(ORIGIN_ADRESS, getDestination());
         }
+
 }
