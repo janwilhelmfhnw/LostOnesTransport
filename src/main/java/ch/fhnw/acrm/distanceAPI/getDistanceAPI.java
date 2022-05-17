bb3278dbbf9a0c5d8410eb09f34406b78c53630e
kenanahmetasevic <kenan.ahmetasevic@students.fhnw.ch>
2022-05-17 17:04:15 +0200

    Distance API connection

--- src/main/java/ch/fhnw/acrm/distanceAPI/getDistanceAPI.java
+++ src/main/java/ch/fhnw/acrm/distanceAPI/getDistanceAPI.java
@@ -1,7 +1,8 @@
 package ch.fhnw.acrm.distanceAPI;
 
+
 import java.net.URI;
-import java.net.http.HttpClient;
+import java.net.http.*;
 import java.net.http.HttpRequest;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -16,18 +17,34 @@ public class getDistanceAPI {
 
         private static final String API_KEY = "AIzaSyCsceVMrBAdupkGbu0zM9jOOBrz2s5wEwY";
         public static float[][] distances;
-        public static float[][] times;
-        public static String[] cities = {"", "Bengaluru", "Chennai", "Goa", "Mumbai", "Hyderabad", "Kolkata", "Patna", "Delhi", "Jaipur,+Rajasthan", "Lukhnow"};
-        public static final int n= cities.length;
 
         //download data
         public static void getData(String source, String destination) throws Exception {
-                String url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + source + "&destinations=" + destination + "&key=" + API_KEY;
-                String request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
-                String client = HttpClient.newBuilder().build();
-                String response = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
-//        System.out.println(response);
-                return response;
+
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(java.net.URI.create("https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + source + "&destinations=" + destination + "&key=" + API_KEY))
+                        .build();
+                client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                        .thenApply(HttpResponse::body)
+                        .thenAccept(System.out::println);
+
+                System.out.println(client.send(request, HttpResponse.BodyHandlers.ofString()).body());
+
+        }
+
+        public static void main(String[] args) {
+
+                String source = "Banglore";
+                String destination = "NewDehli";
+
+                try {
+                        getData(source, destination);
+                } catch (Exception e) {
+                        e.printStackTrace();
+                }
+
+
 
         }
 }
