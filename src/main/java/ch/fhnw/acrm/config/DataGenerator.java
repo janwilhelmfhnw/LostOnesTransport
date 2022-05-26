43ae3d643b5145a1aaed25e2a180003207bb984f
Jan Wilhelm <jan.wilhelm@students.fhnw.ch>
2022-05-26 17:40:26 +0200

    default login

--- src/main/java/ch/fhnw/acrm/config/DataGenerator.java
+++ src/main/java/ch/fhnw/acrm/config/DataGenerator.java
@@ -4,13 +4,12 @@ import ch.fhnw.acrm.business.service.AgentService;
 import ch.fhnw.acrm.data.domain.Agent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import javax.annotation.PostConstruct;
 
-@Profile("test")
+
 @Configuration
-public class DataGeneratorTest {
+public class DataGenerator {
 
     @Autowired
     private AgentService agentService;
@@ -22,8 +21,8 @@ public class DataGeneratorTest {
 
     private void demoUser() throws Exception {
         Agent agentUser = new Agent();
-        agentUser.setEmail("user@user.com");
-        agentUser.setPassword("password");
+        agentUser.setEmail("user@u");
+        agentUser.setPassword("pas");
         agentUser.setName("user");
         agentService.saveAgent(agentUser);
     }
